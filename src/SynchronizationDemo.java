cca32a93f6786ddbf63fbc0b4e29bec9e827f52e
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-20 10:10:11 +0530

    Update SynchronizationDemo.java

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -3,41 +3,32 @@ class Table {
         for (int i = 1; i <= 5; i++) {
             System.out.print(n * i + " ");
         }
-        System.out.println(); // newline after trailing space
+        System.out.println();
     }
 }
-
 class MyThread1 extends Thread {
     Table t;
-
     MyThread1(Table t) {
         this.t = t;
     }
-
     public void run() {
         t.printTable(5);
     }
 }
-
 class MyThread2 extends Thread {
     Table t;
-
     MyThread2(Table t) {
         this.t = t;
     }
-
     public void run() {
         t.printTable(100);
     }
 }
-
-public class SychronizationDemo {
+public class SynchronizationDemo {
     public static void main(String[] args) {
         Table obj = new Table();
-
         MyThread1 t1 = new MyThread1(obj);
         MyThread2 t2 = new MyThread2(obj);
-
         t1.start();
         t2.start();
     }
